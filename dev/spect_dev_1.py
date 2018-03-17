3b377efe9767dbc275b8163ce033a036dc0eaade
Krishna Seegoolam <ks17@cdt063-lap.isd.kcl.ac.uk>
2018-03-17 23:05:33 +0000

    Activity Isolation complete

--- dev/spect_dev_1.py
+++ dev/spect_dev_1.py
@@ -3,7 +3,13 @@ from src.lib import data
 
 Spect = data.Data()
 Spect.import_spect()
+Spect.histogram_analysis()
 Spect.noise_removal()
 
-Visualiser = data.Visualiser(Spect);
+Spect.restore_original_data()
+Visualiser = data.Visualiser(Spect)
+
+
+Spect.restore_calculated_data()
+Visualiser = data.Visualiser(Spect)
 
